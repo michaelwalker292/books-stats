468eedb7579c2bfa607fab21d676f08cd0c548b9
compozedani 11 <compozedani11@gmail.com>
2016-05-12 10:34:07 +0100

    Add initial app

---
+++ public/javascripts/index.jsx
@@ -0,0 +1,11 @@
+'use strict'
+
+var Home = React.createClass(
+  {
+    render: function(){
+      return ( <div>yoyo</div> )
+    }
+  }
+)
+
+ReactDOM.render(<Home />, document.getElementById('entry-point'))
